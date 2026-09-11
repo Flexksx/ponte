50dc99841923e9fecf3bd5fd3b0bb1bc180f64dc
Flexksx <ccretu@extole.com>
2026-09-11 21:43:37 +0300

    update bun handling

--- apps/ponte/src/index.ts
+++ apps/ponte/src/index.ts
@@ -1,4 +1,6 @@
 import { run } from "./cli/run";
 
-const exitCode = await run(Bun.argv.slice(2));
-process.exit(exitCode);
+(async () => {
+  const exitCode = await run(Bun.argv.slice(2));
+  process.exit(exitCode);
+})();
