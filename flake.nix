fc65e3e2b592242922a16b00971c749e5612d658
Flexksx <ccretu@extole.com>
2026-09-18 18:54:24 +0300

    add cachix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,11 @@
 {
   description = "ponte - CLI to sync skills and instructions for AI agent vendors";
 
+  nixConfig = {
+    extra-substituters = ["https://ponte.cachix.org"];
+    extra-trusted-public-keys = ["ponte.cachix.org-1:REQfejqCR1XgRu8BkS1LYEq/jTyjUpY4UZX37svuStk="];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
